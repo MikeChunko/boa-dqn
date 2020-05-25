fe4b72c4a7e357a9099d7f068f5f7383be4b1b2e
MikeChunko <chunkomike@gmail.com>
2020-05-25 13:00:17 -0400

    Bring in line with boa

--- main.py
+++ main.py
@@ -3,111 +3,107 @@ import pygame as pyg
 import random
 
 
-# Returns x, y coords of food
-def gen_food(snake, size_x, size_y, screen_x, screen_y):
-    # Generate new coords until one doesn't cause a collision
-    while True:
-        x, y = (
-            random.randrange(size_x, screen_x - size_x, 10),
-            random.randrange(size_y, screen_y - size_y, 10),
-        )
-        sentinel = True
-        for old_x, old_y in snake:
-            if sentinel and (old_x == x and old_y == y):
-                sentinel = False
-        if sentinel:
-            break
+class Boa:
+    def __init__(self, screen_x = 300, screen_y = 300):
+        pyg.init()
+        pyg.font.init()
+        self.font, self.endfont = pyg.font.SysFont("Arial", 10), pyg.font.SysFont("Arial", 20)
+        self.screen_x, self.screen_y = screen_x, screen_y
+        self.screen = pyg.display.set_mode((self.screen_x, self.screen_y))
+        self.red, self.green, self.blue = (200, 0, 0), (0, 200, 0), (0, 0, 200)
+        self.size_x = self.size_y = 10
+        pyg.display.update()
+        pyg.display.set_caption("Boa")
+        self.clock = pyg.time.Clock()
 
-    return x, y
+        # List containing all segments of the snake
+        self.snake = [(screen_x // 2, screen_y // 2),
+                      (screen_x // 2, screen_y // 2 - self.size_y),
+                      (screen_x // 2, screen_y // 2 - (2 * self.size_y))]
 
-
-pyg.init()
-pyg.font.init()
-font, endfont = pyg.font.SysFont("Arial", 10), pyg.font.SysFont("Arial", 20)
-screen_x = screen_y = 300
-screen = pyg.display.set_mode((screen_x, screen_y))
-red, green, blue = (200, 0, 0), (0, 200, 0), (0, 0, 200)
-size_x = size_y = 10
-pyg.display.update()
-pyg.display.set_caption("Boa")
-clock = pyg.time.Clock()
+        self.gen_food()
+        self.eaten = False
+        self.score = 0
+        self.gameover = False
+        self.d_x, self.d_y = 0, -self.size_y
 
 
-def display(screen, snake, food_x, food_y):
-    screen.fill((0, 0, 0))
-    for x, y in snake:
-        pyg.draw.rect(screen, green, [x, y, size_x, size_y])
-    pyg.draw.rect(screen, red, [food_x, food_y, size_x, size_y])
+    def gen_food(self):
+        # Generate new coords until one doesn't cause a collision
+        while True:
+            x, y = (
+                random.randrange(self.size_x, self.screen_x - self.size_x, 10),
+                random.randrange(self.size_y, self.screen_y - self.size_y, 10),
+            )
+            sentinel = True
+            for old_x, old_y in self.snake:
+                if sentinel and (old_x == x and old_y == y):
+                    sentinel = False
+            if sentinel:
+                break
 
-    for i in range(0, screen_x, 10):  # Border
-        pyg.draw.rect(screen, blue, [i, 0, size_x, size_y])
-        pyg.draw.rect(screen, blue, [i, screen_y - size_y, size_x, size_y])
-        pyg.draw.rect(screen, blue, [0, i, size_x, size_x])
-        pyg.draw.rect(screen, blue, [screen_x - size_x, i, size_x, size_y])
-
-
-# Return 3 tuples, each holding a boolean value in the order (Left, Right, Up, Down)
-# First tuple says if there is a border adjacent to the snake head in that direction
-# Second tuple says if there is a segment adjacent to the snake head in that direction
-# Third tuple says if there is food in that direction
-def get_features(snake, food_x, food_y):
-    b_l = b_r = b_u = b_d = s_l = s_r = s_u = s_d = f_u = f_l = f_r = f_d = False
-    x, y = snake[-1]
-
-    # Border
-    if x == size_x:
-        b_l = True
-    elif x == screen_x - (2 * size_x):
-        b_r = True
-
-    if y == size_y:
-        b_u = True
-    elif y == screen_y - (2 * size_y):
-        b_d = True
-
-    # Segments
-    for x_, y_ in snake[:-1]:
-        if x_ + size_x == x:
-            s_l = True
-        elif x_ - size_x == x:
-            s_r = True
-
-        if y_ + size_y == y:
-            s_u = True
-        elif y_ - size_y == y:
-            s_d = True
-
-    # Food
-    if food_x > x:
-        f_r = True
-    elif food_x < x:
-        f_l = True
-
-    if food_y > y:
-        f_d = True
-    elif food_y < y:
-        f_u = True
-
-    return (b_l, b_r, b_u, b_d), (s_l, s_r, s_u, s_d), (f_l, f_r, f_u, f_d)
+        self.food_x, self.food_y = x, y
 
 
-def run_game():
-    # List containing all segments of the snake
-    snake = [(screen_x // 2, screen_y // 2),
-             (screen_x // 2, screen_y // 2 - size_y),
-             (screen_x // 2, screen_y // 2 - (2 * size_y))]
+    def display(self):
+        self.screen.fill((0, 0, 0))
+        for x, y in self.snake:
+            pyg.draw.rect(self.screen, self.green, [x, y, self.size_x, self.size_y])
+        pyg.draw.rect(self.screen, self.red, [self.food_x, self.food_y, self.size_x, self.size_y])
 
-    food_x, food_y = gen_food(snake, size_x, size_y, screen_x, screen_y)
-    eaten = False
-    score = 0
-    gameover = False
-    d_x, d_y = 0, -size_y
+        for i in range(0, self.screen_x, 10):  # Border
+            pyg.draw.rect(self.screen, self.blue, [i, 0, self.size_x, self.size_y])
+            pyg.draw.rect(self.screen, self.blue, [i, self.screen_y - self.size_y, self.size_x, self.size_y])
+            pyg.draw.rect(self.screen, self.blue, [0, i, self.size_x, self.size_x])
+            pyg.draw.rect(self.screen, self.blue, [self.screen_x - self.size_x, i, self.size_x, self.size_y])
 
-    # Main loop
 
-    while not gameover:
+    # Return 3 tuples, each holding a boolean value in the order (Left, Right, Up, Down)
+    # First tuple says if there is a border adjacent to the snake head in that direction
+    # Second tuple says if there is a segment adjacent to the snake head in that direction
+    # Third tuple says if there is food in that direction
+    def get_features(self):
+        b_l = b_r = b_u = b_d = s_l = s_r = s_u = s_d = f_u = f_l = f_r = f_d = False
+        x, y = self.snake[-1]
 
-        # Handle actions
+        # Border
+        if x == self.size_x:
+            b_l = True
+        elif x == self.screen_x - (2 * self.size_x):
+            b_r = True
+
+        if y == self.size_y:
+            b_u = True
+        elif y == self.screen_y - (2 * self.size_y):
+            b_d = True
+
+        # Segments
+        for x_, y_ in self.snake[:-1]:
+            if x_ + self.size_x == x:
+                s_l = True
+            elif x_ - self.size_x == x:
+                s_r = True
+
+            if y_ + self.size_y == y:
+                s_u = True
+            elif y_ - self.size_y == y:
+                s_d = True
+
+        # Food
+        if self.food_x > x:
+            f_r = True
+        elif self.food_x < x:
+            f_l = True
+
+        if self.food_y > y:
+            f_d = True
+        elif self.food_y < y:
+            f_u = True
+
+        return (b_l, b_r, b_u, b_d), (s_l, s_r, s_u, s_d), (f_l, f_r, f_u, f_d)
+
+
+    def get_keyboard_input(self):
         for event in pyg.event.get():
             if event.type == pyg.QUIT:
                 pyg.quit()
@@ -115,58 +111,75 @@ def run_game():
             # Movement keys
             if event.type == pyg.KEYDOWN:
                 if (event.key == pyg.K_LEFT or event.key == pyg.K_a):
-                    if d_x <= 0:
-                        d_x, d_y = -size_x, 0
+                    self.get_input(0)
                 elif (event.key == pyg.K_RIGHT or event.key == pyg.K_d):
-                    if d_x >= 0:
-                        d_x, d_y = size_x, 0
+                    self.get_input(1)
                 elif (event.key == pyg.K_UP or event.key == pyg.K_w):
-                    if d_y <= 0:
-                        d_x, d_y = 0, -size_y
+                    self.get_input(2)
                 elif (event.key == pyg.K_DOWN or event.key == pyg.K_s):
-                    if d_y >= 0:
-                        d_x, d_y = 0, size_y
+                    self.get_input(3)
+
+
+    def get_input(self, input):
+        if input == 0 and self.d_x <= 0:
+            self.d_x, self.d_y = -self.size_x, 0
+        elif input == 1 and self.d_x >= 0:
+            self.d_x, self.d_y = self.size_x, 0
+        elif input == 2 and self.d_y <= 0:
+            self.d_x, self.d_y = 0, -self.size_y
+        elif input == 3 and self.d_y >= 0:
+            self.d_x, self.d_y = 0, self.size_y
+
+
+    # -1 for keyboard input, 0 for left, 1 for right, 2 for up, 3 for down
+    def step(self, tick=15, input=-1):
+        # Handle actions
+        if input == -1:
+            self.get_keyboard_input()
+        else:
+            self.get_input(input)
 
         # New segment position
-        x, y = snake[-1]
-        new_x, new_y = x + d_x, y + d_y
+        x, y = self.snake[-1]
+        new_x, new_y = x + self.d_x, y + self.d_y
 
         # Check collisions
-        if new_x == food_x and new_y == food_y:  # Food
-            eaten = True
-        elif new_x == 0 or new_x == screen_x - size_x or new_y == 0 or new_y == screen_y - size_y:  # Border
-            gameover = True
-        if d_x != 0 or d_y != 0:
-            for x, y in snake:  # Snake
+        if new_x == self.food_x and new_y == self.food_y:  # Food
+            self.eaten = True
+        elif new_x == 0 or new_x == self.screen_x - self.size_x or new_y == 0 or new_y == self.screen_y - self.size_y:  # Border
+            self.gameover = True
+        if self.d_x != 0 or self.d_y != 0:
+            for x, y in self.snake:  # Snake
                 if new_x == x and new_y == y:
-                    gameover = True
+                    self.gameover = True
 
-        snake.append((new_x, new_y))
+        self.snake.append((new_x, new_y))
 
         # Food is eaten
-        if eaten:
-            food_x, food_y = gen_food(snake, size_x, size_y, screen_x, screen_y)
-            eaten = False
-            score += 100
+        if self.eaten:
+            self.gen_food()
+            self.eaten = False
+            self.score += 100
         else:
-            if score > 0:
-                score -= 1
-            snake.pop(0)
+            if self.score > 0:
+                self.score -= 1
+            self.snake.pop(0)
 
         # Draw
-        display(screen, snake, food_x, food_y)
-        textsurface = font.render("Score: {}".format(score), False, (255, 255, 255))
-        screen.blit(textsurface, (0, 0))
+        self.display()
+        textsurface = self.font.render("Score: {}".format(self.score), False, (255, 255, 255))
+        self.screen.blit(textsurface, (0, 0))
 
         pyg.display.update()
-        clock.tick(15)
-
-    return snake, score, (food_x, food_y)
+        self.clock.tick(tick)
 
 
 if __name__ == "__main__":
+    tick = 15
     while True:
-        snake, score, (food_x, food_y) = run_game()
+        game = Boa()
+        while not game.gameover:
+            game.step(tick=tick, input=-1)
 
         # Game over state
         sentinel = True
@@ -178,11 +191,12 @@ if __name__ == "__main__":
                 if event.type == pyg.KEYDOWN and event.key == pyg.K_r:
                     sentinel = False
 
-            display(screen, snake, food_x, food_y)
-            textsurface = endfont.render("Game Over! Score: {}".format(score), True, (255, 255, 255))
-            screen.blit(textsurface, textsurface.get_rect(center=(screen_x // 2, screen_y // 2)))
-            textsurface = endfont.render("Press R to restart", True, (255, 255, 255))
-            screen.blit(textsurface, textsurface.get_rect(center=(screen_x // 2, screen_y // 2 + 20)))
+            game.display()
+            textsurface = game.endfont.render("Game Over! Score: {}".format(game.score), True, (255, 255, 255))
+            game.screen.blit(textsurface, textsurface.get_rect(center=(game.screen_x // 2, game.screen_y // 2)))
+            textsurface = game.endfont.render("Press R to restart", True, (255, 255, 255))
+            game.screen.blit(textsurface, textsurface.get_rect(center=(game.screen_x // 2, game.screen_y // 2 + 20)))
+
 
             pyg.display.update()
-            clock.tick(15)
+            game.clock.tick(tick)
